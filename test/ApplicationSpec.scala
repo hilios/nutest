0410f6a7be79e8db51bdcac0c8cb1fa384f9b78f
Edson Hilios <edson.hilios@unear.net>
2016-05-31 20:50:36 -0300

    Remove cors test

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -56,12 +56,6 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
       contentAsString(home) must include ("version")
       contentAsString(home) must include ("name")
     }
-
-    "include cross-origin resource sharing" in {
-      val home = route(app, FakeRequest(GET, "/")).get
-
-      headers(home) must contain ("Access-Control-Allow-Origin" -> "*")
-    }
   }
 
   "RewardController" when {
