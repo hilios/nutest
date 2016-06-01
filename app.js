2f97ec3d241e58e78ae0f4c8ee91093cac4735d9
Edson Hilios <edson.hilios@unear.net>
2016-06-01 14:53:48 -0300

    Fix typos made at the refactor

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('nutest', ['ngResource', 'ui.bootstrap', 'angularFileUpload']);
+var app = angular.module('nutest', ['ngResource', 'ui.bootstrap']);
 
 /**
  * Services
@@ -29,7 +29,7 @@ app.factory('Reward', function($resource, ApiUrl) {
  * Controllers
  */
 app.controller('StatusController', function($scope, Status, ApiUrl) {
-  $scope.version = '*.*.*';
+  $scope.version = '0.0.0';
   $scope.url = ApiUrl;
 
   Status.get(function(data) {
@@ -57,7 +57,7 @@ app.controller('RewardController', function($scope, $uibModal, Reward) {
   }
 });
 
-app.controller('FormController', function($scope, $uibModalInstance, FileUploader, ApiUrl, Reward) {
+app.controller('FormController', function($scope, $uibModalInstance, Reward) {
   $scope.invites = "";
   $scope.invitationFile = undefined;
 
@@ -67,14 +67,12 @@ app.controller('FormController', function($scope, $uibModalInstance, FileUploade
     if ($scope.invitationFile) {
       var data = new FormData();
       data.append('invites', $scope.invitationFile);
-      // TODO: File upload
+      // File upload
       Reward.create(data, function() {
         $uibModalInstance.close(true);
       }, function() {
         form.$setValidity('invites', false);
       });
-      console.log("upload", $scope.invitationFile);
-
     } else {
       Reward.update($scope.invites.trim(), function() {
         $uibModalInstance.close(true);
@@ -102,49 +100,3 @@ app.directive('fileInput', function() {
     }
    };
  });
-
- /*
- var uploader = new FileUploader({url: ApiUrl + '/rewards'})
-
- // FILTERS
- uploader.filters.push({
-     name: 'customFilter',
-     fn: function(item /*{File|FileLikeObject}* /, options) {
-         return this.queue.length < 10;
-     }
- });
-
- uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}* /, filter, options) {
-   console.info('onWhenAddingFileFailed', item, filter, options);
- };
- uploader.onAfterAddingFile = function(fileItem) {
-   console.info('onAfterAddingFile', fileItem);
- };
- uploader.onAfterAddingAll = function(addedFileItems) {
-   console.info('onAfterAddingAll', addedFileItems);
- };
- uploader.onBeforeUploadItem = function(item) {
-   console.info('onBeforeUploadItem', item);
- };
- uploader.onProgressItem = function(fileItem, progress) {
-   console.info('onProgressItem', fileItem, progress);
- };
- uploader.onProgressAll = function(progress) {
-   console.info('onProgressAll', progress);
- };
- uploader.onSuccessItem = function(fileItem, response, status, headers) {
-   console.info('onSuccessItem', fileItem, response, status, headers);
- };
- uploader.onErrorItem = function(fileItem, response, status, headers) {
-   console.info('onErrorItem', fileItem, response, status, headers);
- };
- uploader.onCancelItem = function(fileItem, response, status, headers) {
-   console.info('onCancelItem', fileItem, response, status, headers);
- };
- uploader.onCompleteItem = function(fileItem, response, status, headers) {
-   console.info('onCompleteItem', fileItem, response, status, headers);
- };
- uploader.onCompleteAll = function() {
-     console.info('onCompleteAll');
- };
- */
