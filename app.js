092ae4518503de01a7eef495b6f362763515108e
Edson Hilios <edson.hilios@unear.net>
2016-06-05 12:52:55 -0300

    Improve UX

--- app.js
+++ app.js
@@ -60,10 +60,13 @@ app.controller('RewardController', function($scope, $uibModal, Reward) {
   }
 
   $scope.delete = function() {
-    var confirmed = confirm('Are you sure? This can not be undone.');
-    if (confirmed) {
+    var modal = $uibModal.open({
+      templateUrl: 'confirm.html',
+    });
+    // Delete
+    modal.result.then(function() {
       Reward.delete($scope.load);
-    }
+    });
   }
 });
 
@@ -110,3 +113,84 @@ app.directive('fileInput', function() {
     }
    };
  });
+
+ /**
+ * Renders a HTML loader indicator that responds to $rootScope events
+ * broadcasted by HTTP interceptors.
+ */
+app.directive('spinner', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'spinner.html',
+    link: function(scope, element) {
+      scope.displayLoading = false;
+      scope.displayError = false;
+      // Prevent clicks at the interface
+      element.on('click', function(event) {
+        // Only in root node, aka overlay
+        if (event.target !== this) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+      });
+      // Show spiner and hide error
+      scope.$on('loading:start', function() {
+        scope.displayLoading = true;
+        scope.displayError = false;
+      });
+      // Hide spiner
+      scope.$on('loading:complete', function() {
+        scope.displayLoading = false;
+      });
+      // Hide spiner and show error
+      scope.$on('loading:error', function(event, rejection) {
+        if (rejection && (rejection.status === 400)) {
+          scope.displayLoading = false;
+        } else {
+          scope.displayError = true;
+        }
+      });
+    }
+  };
+});
+
+/**
+ * Intercept all HTTP requests in the app and dispatches an loading event.
+ * @requires $httpProvider
+ */
+app.config(function($httpProvider) {
+  var TEMPLATE_REGEXP = /\.html$/i;
+  $httpProvider.interceptors.push(function($q, $timeout, $rootScope) {
+    return {
+      request: function(config) {
+        if (!TEMPLATE_REGEXP.test(config.url)) {
+          // Defer to when function exits
+          $timeout(function() {
+            $rootScope.$broadcast('loading:start');
+          });
+        }
+        return config;
+      },
+      response: function(response) {
+        if (!TEMPLATE_REGEXP.test(response.config.url)) {
+          $timeout(function() {
+            $rootScope.$broadcast('loading:complete');
+          });
+        }
+        return response;
+      },
+      requestError: function(rejection) {
+        $timeout(function() {
+          $rootScope.$broadcast('loading:error', rejection);
+        });
+        return $q.reject(rejection);
+      },
+      responseError: function(rejection) {
+        $timeout(function() {
+          $rootScope.$broadcast('loading:error', rejection);
+        });
+        return $q.reject(rejection);
+      }
+    };
+  });
+});
